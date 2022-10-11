c4ab562fbe24c0ff2e5a3809e3293b26da749a19
Grace Tian <grace.tian@duke.edu>
2022-10-11 16:44:19 -0400

    added dataset scaling

--- experiment1.py
+++ experiment1.py
@@ -3,31 +3,39 @@ import pandas as pd
 import numpy as np
 import math
 import tensorflow as tf
+import normalize
 
-WINDOW_SIZE = 50  # number of time frames for one prediction
 LABEL = 15  # sensor 17 corresponds to index 15 after dropping sensor 6 due to missing information
+WINDOW_SIZE = 50  # number of time frames for one prediction
 NUM_EPOCHS = 2  # specify number of epochs to train over
 BATCH_SIZE = 32  # specify batch size 
 
 # generate time series from csv data files
 directory = "db1"
 dataset = [] # list of all (data, label) pairs
+scaled_dataset = [] # list of all scaled (data, label) pairs
+fcount = 0 
 print("Generating dataset...")
 for file in os.listdir(directory):
+    if fcount == 2: break
     subject = file.split('_')[0][1:]
     f = os.path.join(directory, file)
     df = pd.read_csv(f)
     df = df.drop(columns='5')
     data = np.array(df, dtype=np.float32)
+    scaled_data = normalize.scale_to_range(data, -1, 1)
     for row in range(len(data) - (WINDOW_SIZE + 1)):
-        dataset.append((data[row:row + WINDOW_SIZE], data[row + WINDOW_SIZE + 1][LABEL]))  # (data, label) pair
+        dataset.append((np.delete(data[row:row + WINDOW_SIZE], LABEL, 1), data[row + WINDOW_SIZE + 1][LABEL]))  # (data, label) pair
+        scaled_dataset.append((np.delete(scaled_data[row:row + WINDOW_SIZE], LABEL, 1), scaled_data[row + WINDOW_SIZE + 1][LABEL]))  # scaled (data, label) pair
     print("Processed " + f)
+    break
+    fcount+=1
 print("Generating dataset... done! :)")
 
-# create training and testing datasets
+# create training and testing datasets from scaled_dataset
 print("Generating training and testing datasets...")
-random_data = np.random.permutation(len(dataset))
-cutoff = math.floor(len(dataset) * 0.8)
+random_data = np.random.permutation(len(scaled_dataset))
+cutoff = math.floor(len(scaled_dataset) * 0.8)
 training_dataset, testing_dataset = random_data[:cutoff], random_data[cutoff:]
 print("Generating training and testing datasets... done! :)")
 
@@ -35,8 +43,8 @@ print("Generating training and testing datasets... done! :)")
 model = tf.keras.Sequential([
     # Shape: (time, features) => (time*features)
     tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(units=32, activation='relu'),
-    tf.keras.layers.Dense(units=32, activation='relu'),
+    tf.keras.layers.Dense(units=32, activation='tanh'),
+    tf.keras.layers.Dense(units=32, activation='tanh'),
     tf.keras.layers.Dense(units=1),
     # Add back the time dimension.
     # Shape: (outputs) => (1, outputs)
@@ -78,5 +86,7 @@ print("test loss, test acc:", results)
 print("Generating predictions...")
 predictions = model.predict(testing_features[:10])
 print("Predictions: ", predictions)
-print("Ground Truth: ", testing_labels[:10])
+print("Ground Truth", testing_labels[:10])
+# print("Predictions: ", normalize.unscale_from_range(predictions, dataset, -1, 1))
+# print("Ground Truth: ", normalize.unscale_from_range(testing_labels[:10], dataset, -1, 1))
 print("Generating predictions... done! :)")
---
+++ normalize.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def scale_to_range(data, left_bound, right_bound):
+    return ((data - np.min(data)) / (np.max(data) - np.min(data))) * (right_bound - left_bound) + left_bound
+
+def unscale_from_range(data, original_data, left_bound, right_bound):
+    min_val = float('inf')
+    max_val = float('-inf')
+    for i in range(len(original_data)):
+        for j in range(len(original_data[i])):
+            min_val = min(min_val, np.min(original_data[i][j]))
+            max_val = max(max_val, np.max(original_data[i][j]))
+    return ((data - left_bound) / (right_bound - left_bound)) * (max_val - min_val) + min_val
+
+"""
+TESTING
+"""
+# data = [[5, 8, -1, 3], [-10, 2, -7, 6], [4, 1, -9, 5]]
+# data = [[5], [8], [-1], [3]]
+# data = np.array(data)
+# scaled_data = scale_to_range(data, -1, 1)
+# unscaled_data = unscale_from_range(scaled_data, data, -1, 1)
+# print(data)
+# print(scaled_data)
+# print(unscaled_data)

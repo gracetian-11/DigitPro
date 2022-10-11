a67283a6453de6feb1b3da3d4d24011172eb2826
Grace Tian <grace.tian@duke.edu>
2022-10-11 00:19:22 -0400

    data processing for experiment 1

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # DigitPro
 Machine Learning for Prosthetic Finger Kinematics
+
+Experiment 1:
+
+Our dataset is composed of all windows of 50 time frames over the 27 subjects in db1. We randomly select 80% of these windows for training and the remaining 20% for testing.
+
+We assume amputation of the tip of the second (pointer) finger so we remove information from sensor 17 from the input. Our model should predict the information from the missing sensors. As a test of feasibility, the neural network takes as input the data from all sensors except 17 over 50 time frames and predicts the projected value for sensor 17 at the first time frame after the given input.
---
+++ experiment1.py
@@ -0,0 +1,30 @@
+import os
+import pandas as pd
+import numpy as np
+import math
+import tensorflow as tf
+
+WINDOW_SIZE = 50  # number of time frames for one prediction
+LABEL = 15  # sensor 17 corresponds to index 15 after dropping sensor 6 due to missing information
+
+# generate time series from csv data files
+directory = "db1"
+dataset = [] # list of all (data, label) pairs
+print("Generating dataset...")
+for file in os.listdir(directory):
+    subject = file.split('_')[0][1:]
+    f = os.path.join(directory, file)
+    df = pd.read_csv(f)
+    df = df.drop(columns='5')
+    data = np.array(df, dtype=np.float32)
+    for row in range(len(data) - (WINDOW_SIZE + 1)):
+        dataset.append((data[row:row + WINDOW_SIZE], data[row:row + WINDOW_SIZE + 1][LABEL]))  # (data, label) pair
+    print("Processed " + f)
+print("Generating dataset... done! :)")
+
+# create training and testing datasets
+print("Generating training and testing datasets...")
+random_data = np.random.permutation(len(dataset))
+cutoff = math.floor(len(dataset) * 0.8)
+training_dataset, testing_dataset = random_data[:cutoff], random_data[cutoff:]
+print("Generating training and testing datasets... done! :)")
